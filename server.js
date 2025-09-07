b5c642e974820435b5ac70d64ee8c66f3ce1b70f
sreekanth <imaginations420@gmail.com>
2025-09-07 21:31:04 +0530

    test

--- server.js
+++ server.js
@@ -8,10 +8,23 @@ const cors = require('cors');
 const app = express();
 const PORT = 5000;
 
+const allowedOrigins = [
+  "http://localhost:3000",
+  "https://kclicks-frontend.netlify.app",
+  "https://kclicks-frontend2.vercel.app"
+];
+
 app.use(cors({
-  origin: 'https://kclicks-frontend.netlify.app/',
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
   credentials: true
 }));
+
 
 app.use(express.json());
 app.use(cookieParser());
