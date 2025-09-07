ff9649893a60172f672c96240df2424c3e892873
sreekanth <imaginations420@gmail.com>
2025-09-07 21:26:29 +0530

    fixed cors issue

--- server.js
+++ server.js
@@ -8,6 +8,11 @@ const cors = require('cors');
 const app = express();
 const PORT = 5000;
 
+app.use(cors({
+  origin: 'https://kclicks-frontend.netlify.app/',
+  credentials: true
+}));
+
 app.use(express.json());
 app.use(cookieParser());
 app.use(session({
@@ -22,8 +27,3 @@ app.use('/api/auth', authRoutes);
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
 });
-
-app.use(cors({
-  origin: 'https://kclicks-frontend.netlify.app/',
-  credentials: true
-}));
